667d90994af6416559fa197b2f3e9d8b63a53b15
slavni <slavnipokusaj@gmail.com>
2020-05-26 15:24:42 +0200

    nista opet

--- Index.js
+++ Index.js
@@ -220,6 +220,7 @@ app.post("/dodajVrednostRashoda", function (req, res) {
 });*/
 app.post("/dodajVrednostRashoda/datum/:datum/vrednost/:vrednost/ID_Vrste_Rashoda/:ID_Vrste_Rashoda", function (req, res) {        
     res.send('post 1 dodat'); 
+    res.send(req.params.datum);
     //let sql = 'INSERT INTO lista_rashoda (Datum,Vrednost,ID_Vrste_Rashoda) values ("'+req.params.datum +'", "'+ req.params.vrednost +'","'+ req.params.ID_Vrste_Rashoda +'")';
     //let query = db.query(sql, (err,result)=>{
       //  if(err) throw err;
